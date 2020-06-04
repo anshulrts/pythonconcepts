2a49aa111378ac6681287357fc45a3f38bb7e9b5
anshulrts <anshul.rts@gmail.com>
2020-06-04 23:03:26 +0530

    Understanding Importing of Modules

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def start():
-    print("Hello")
+import understandingimport
 
+def importingmodules():
+    understandingimport.start()
+
+switcher = {
+    1: importingmodules
+}
+
+def start():
+    while(True):
+        print("       Menu")
+        print(" 1. Importing Modules")
+
+        ch = input("Enter choice : ")
+        callmethod = switcher.get(int(ch), lambda: print('Invalid'))
+        callmethod()
 
 if __name__ == "__main__":
     start()
