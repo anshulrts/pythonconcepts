ec22a0bbeb0d66e1d75710c830012468956f764b
Anshul Dahiya <anshul.rts@gmail.com>
2020-07-15 16:41:12 +0530

    Added Linked List

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import understanding_import
 import global_local_package.global_local_variable
+import linked_list
 
 def importing_modules():
     understanding_import.start()
@@ -7,9 +8,13 @@ def importing_modules():
 def global_local():
     global_local_package.global_local_variable.start()
 
+def linked_list():
+    linked_list.start()
+
 switcher = {
     1: importing_modules,
-    2: global_local
+    2: global_local,
+    3: linked_list
 }
 
 def start():
@@ -17,6 +22,7 @@ def start():
         print("       Menu")
         print(" 1. Importing Modules")
         print(" 2. Global vs Local Variable")
+        print(" 3. Linked List")
 
         ch = input("Enter choice : ")
         call_method = switcher.get(int(ch), lambda: print('Invalid'))
