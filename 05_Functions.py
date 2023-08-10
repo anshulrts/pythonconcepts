bb4d1b66976ae38e8c08e0797143460df1c8a44d
anshul.dahiya <anshul.rts@gmail.com>
2023-08-10 22:10:38 +0530

    Functions

---
+++ 05_Functions.py
@@ -0,0 +1,22 @@
+# Functions in Python start with def keyword
+# The return value of a Python function can be any object.
+# Everything in python is an object. So you functions can return numeric, collections, user
+# defined objects, classes, functions even modules & packages.
+# They always return a value, even if you don't specify a return statement
+
+# You can omit the return value of a function and use a bare return without a return value.
+# You can also omit the entire return statement. In both cases, the return value will be None.
+
+# Returning Multiple Values
+# In Python, you can return multiple values. You just need to separate them by comma.
+
+def returnmultiple():
+    return 1, 2, 3
+
+desc = returnmultiple()
+print(desc)
+# desc becomes a tuple here storing all 3 values
+
+a, b, c = returnmultiple()
+print(a,b,c)
+# This concept of unpacking in 3 variables is called a iterable unpacking.
