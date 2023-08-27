a82174b3caeb0fc8a0ed8e84773550dd5b103bf4
anshul.dahiya <anshul.rts@gmail.com>
2023-08-27 11:47:40 +0530

    main

--- 12_main.py
+++ 12_main.py
@@ -4,3 +4,27 @@
 # Having a defined starting point for the execution of a program is useful for
 # understanding how a program works. Python programmers have come up with several conventions
 # to define this starting point.
+
+# Article Link - https://realpython.com/python-main-function/
+
+# Best way to create python files:
+
+from time import sleep
+
+print("This is my file to demonstrate best practices.")
+
+def process_data(data):
+    print("Begin Processing...")
+    modified_data = data + " that has been modified"
+    sleep(3)
+    print("Data processing finished.")
+    return modified_data
+
+def main():
+    data = "My data read from the web"
+    print(data)
+    modified_data = process_data(data)
+    print(modified_data)
+
+if __name__ == '__main__':
+    main()
