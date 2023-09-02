f6efa51384d7b9b6272541e93daccd07d6c67ec0
anshul.dahiya <anshul.rts@gmail.com>
2023-09-02 14:20:39 +0530

    01

--- 01_types.py
+++ 01_types.py
@@ -11,24 +11,24 @@
 
 # Eg
 x = 5
-print(type(x))
+print(type(x)) # <class 'int'>
 y = 'abc'
-print(type(y))
+print(type(y)) # <class 'str'>
 # We can point to a new type
 y = 10
-print(type(y))
+print(type(y)) # <class 'int'>
 
 # NoneType
 z = None
-print(type(z))
+print(type(z)) # <class 'NoneType'>
 
 # String type
 string1 = "This is a string"
 
-print(f"Length of string1 is : {len(string1)}")
-print(f"First Character is : {string1[0]}")
+print(f"Length of string1 is : {len(string1)}") # Length of string1 is : 16
+print(f"First Character is : {string1[0]}") # First Character is : T
 # Negative Indexing
-print(f"Last Character is : {string1[-1]}")
+print(f"Last Character is : {string1[-1]}") # Last Character is : g
 
 # In Python, you can pack & unpack tuples.
 # Eg
@@ -37,7 +37,7 @@ s2 = 2
 s3 = 3
 s4 = 4
 tt = (s1, s2, s3, s4)
-print(tt)
+print(tt) # (1, 2, 3, 4)
 
 (x, y, z, a) = tt
-print(x)
+print(x) # 1
