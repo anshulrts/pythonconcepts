17c9e9077cb65bd56d83cbc2f4c5d8164bbe6d26
anshul.dahiya <anshul.rts@gmail.com>
2023-09-04 22:06:18 +0530

    comments

--- 02_types_conversions.py
+++ 02_types_conversions.py
@@ -7,20 +7,22 @@
 x = 10
 y = 1.23
 z = x + y
-print(f"Value : {z}")
+print(f"Value : {z}") # 11.23
 print(f"Type : {type(z)}") # Python always convert smaller type to bigger one to avoid data loss
+# Type : <class 'float'>
+
 
 # Explicit - Also called as Type Casting
 # We get a TypeError when we try to add a str and int directly, because implicit doesn't work here
 x = 10
 y = '12'
 z = x + int(y)
-print(f"Value : {z}")
-print(f"Type : {type(z)}")
+print(f"Value : {z}") # Value : 22
+print(f"Type : {type(z)}") # Type : <class 'int'>
 
 x = 10
 y = 1.23
 z = x + int(y)
-print(f"Value : {z}")
-print(f"Type : {type(z)}")
+print(f"Value : {z}") # Value : 11
+print(f"Type : {type(z)}") # Type : <class 'int'>
 # Note - There might be a data loss during explicit conversion
